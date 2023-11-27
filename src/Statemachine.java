01bed18e618d939ac4d9f13538ffcdc0ccb709e0
Ryan Hanley <camelCaseGuy@users.noreply.github.com>
2023-11-26 21:49:00 -0600

    fixed math, ready to submit

--- src/Statemachine.java
+++ src/Statemachine.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 abstract class State {
   static State initialize, accumulate, subtract, add, equalize, error, current;
 
+  static int total = 0; // Specify the data type as int
+  static int currentInput = 0; // Specify the data type as int
+  static String symbol = "";
+
   void enter() {
   };
 
@@ -16,15 +20,20 @@ class Initialize extends State {
   void enter() {
     System.out.println("Welcome to the calculator. Now in Initialize state.");
     System.out.println("Enter a character, 1-9: ");
+    currentInput = 0;
+    total = 0;
   }
 
   void update(Scanner scanner) {
     String input = scanner.next();
     if (Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9").contains(input)) {
       current = accumulate;
+      currentInput = Integer.parseInt(input);
     } else {
       current = error;
     }
+    State.current.enter();
+    State.current.update(scanner);
   }
 }
 
@@ -37,7 +46,7 @@ class Accumulate extends State {
   void update(Scanner scanner) {
     String input = scanner.next();
     if (Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9").contains(input)) {
-      current = accumulate;
+      currentInput = currentInput * 10 + Integer.parseInt(input);
     } else if (input.equals("-")) {
       current = subtract;
     } else if (input.equals("+")) {
@@ -48,53 +57,103 @@ class Accumulate extends State {
       current = error;
     }
 
+    
+    // if (symbol.equals("-")) {
+    //   total = total - currentInput;
+    // } else if (symbol.equals("+")) {
+    //   total = total + currentInput;
+    // } else if (symbol.equals("")) {
+    //   total = currentInput;
+    // }
+
+    // currentInput = 0;
+    
+    State.current.enter();
+    State.current.update(scanner);
   }
 }
 
 class Subtract extends State {
   void enter() {
     System.out.println("Now in Subtract state.");
+    if (symbol.equals("-")) {
+      total = total - currentInput;
+    } else if (symbol.equals("+")) {
+      total = total + currentInput;
+    } else {
+      total = currentInput;
+    }
+    symbol = "-";
     System.out.println("Enter a character, 0-9 : ");
   }
 
   void update(Scanner scanner) {
     String input = scanner.next();
+
     if (Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9").contains(input)) {
+      currentInput = Integer.parseInt(input);
       current = accumulate;
     } else {
       current = error;
     }
+    State.current.enter();
+    State.current.update(scanner);
   }
 }
 
 class Add extends State {
   void enter() {
     System.out.println("Now in Add state.");
+
+    if (symbol.equals("-")) {
+      total = total - currentInput;
+    } else if (symbol.equals("+")) {
+      total = total + currentInput;
+    } else {
+      total = currentInput;
+    }
+
+    symbol = "+";
+    
+    System.out.println("Total so far: " + total);
     System.out.println("Enter a character, 0-9 : ");
   }
 
   void update(Scanner scanner) {
     String input = scanner.next();
     if (Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9").contains(input)) {
+      currentInput = Integer.parseInt(input);
       current = accumulate;
     } else {
       current = error;
     }
+    State.current.enter();
+    State.current.update(scanner);
   }
 }
 
 class Equalize extends State {
   void enter() {
     System.out.println("Now in Equalize state.");
-    System.out.println("Your evaluation: ");
+    if (symbol.equals("-")) {
+      total = total - currentInput;
+    } else if (symbol.equals("+")) {
+      total = total + currentInput;
+    } else if (symbol.equals("")) {
+      total = currentInput;
+    }
+    symbol = "";
+    System.out.println("Your total: " + total);
     System.out.println("Press Y to do another calculation, or any other key to exit : ");
 
   }
 
   void update(Scanner scanner) {
     String input = scanner.next();
-    if (input.equals("Y")) {
+    if (input.toLowerCase().equals("y")) {
       current = initialize;
+      State.current.enter();
+      State.current.update(scanner);
     } else {
       System.exit(0);
     }
@@ -105,10 +164,18 @@ class Error extends State {
   void enter() {
     System.out.println("Now in Error state.");
     System.out.println("Sorry, you have an error. Please follow the instructions.");
+    System.out.println("Press Y to do another calculation, or any other key to exit : ");
   }
 
   void update(Scanner scanner) {
-    current = initialize;
+    String input = scanner.next();
+    if (input.equals("Y")) {
+      current = initialize;
+      State.current.enter();
+      State.current.update(scanner);
+    } else {
+      System.exit(0);
+    }
   }
 }
 
@@ -122,10 +189,8 @@ public class Statemachine {
     State.error = new Error();
     State.current = State.initialize;
     Scanner scanner = new Scanner(System.in);
-    while (true) {
-      State.current.enter();
-      State.current.update(scanner);
-    }
+    State.current.enter();
+    State.current.update(scanner);
 
   }
 }
